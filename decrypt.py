2384f41b524289b3f605a246857a4c101f512f79
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2020-10-31 09:55:25 +0000

    Up to unit 3

---
+++ decrypt.py
@@ -0,0 +1,10 @@
+
+def chant(phrase):
+    # Glue three copies together and print it as a message
+    print(phrase + phrase + phrase)
+
+
+# Associate variable diana with the value "WONDER WOMAN 1984"
+diana = "WONDER WOMAN 1984"
+
+chant("I believe Diana is actually " + diana)
